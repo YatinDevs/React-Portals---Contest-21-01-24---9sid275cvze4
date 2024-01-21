3e26e9dc97da023eecdfe878e74a2619739c743a
Yatin Chaudhari <c.yatin727@gmail.com>
2024-01-21 05:35:57 +0000

    chnages

--- src/components/PortalTextArea.js
+++ src/components/PortalTextArea.js
@@ -1,13 +1,26 @@
-import React from 'react';
-import  ReactDOM  from 'react-dom';
+import React, { useState } from "react";
+import ReactDOM, { createPortal } from "react-dom";
 
 //complete this function using portals
-const PortalTextArea=()=>{
-    return (
-        <div>
-            <textarea id="textarea" value={}></textarea>
-        </div>
-    )
-}
+const PortalTextArea = ({textareaRef}) => {
+//   const [textValue, setTextValue] = useState("");
+
+
+  const portalRoot = document.getElementById("portal-textarea");
+  return portalRoot
+    ? ReactDOM.createPortal(
+        <textarea
+        ref={textareaRef}
+          id="textarea"
+          
+          onChange={(e) => {
+            handleTextChange(e.target.value);
+          }}
+        />,
+
+        portalRoot
+      )
+    : null;
+};
 export default PortalTextArea;
 //portal-textarea id will be used here for portal purpose
